5f3e449d2788af966b37689d5a203bde358ed4c9
srijansam <93420738+srijansam@users.noreply.github.com>
2024-01-16 12:55:15 +0530

    Create 2011. Final Value of Variable After Performing Operations.cpp

---
+++ 2011. Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for (auto& operation : operations) {
+            if (operation[1] == '+') x++;
+            else x--;
+        }
+        return x;
+    }
+};
